10f028cedab36b0443c2ac662f12a91b3379e8a3
Bryant <bryant-m@hotmail.com>
2022-12-08 20:07:21 -0400

    first commit

---
+++ APItestStudent/Controllers/StudentsController.cs
@@ -0,0 +1,50 @@
+﻿using APItestStudent.Entities;
+using APItestStudent.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace APItestStudent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+       private readonly IService<Student> students;
+
+        public StudentsController(IService<Student> service)
+        {
+            students = service;
+        }
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {
+            return students.GetAll();
+        }
+        [HttpGet("{id}")]
+        public Student GetById([FromRoute] Guid id)
+        {
+           return students.GetById(id);
+        }
+        [HttpPost]
+        public IActionResult Add([FromBody]Student value)
+        {
+            students.Add(value);
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Update([FromBody] Student value)
+        {
+            students.Update(value);
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Remove([FromRoute] Guid id)
+        {
+            students.Remove(id);
+            return Ok();
+        }
+    }
+}
---
+++ APItestStudent/Services/IService.cs
@@ -0,0 +1,54 @@
+﻿using APItestStudent.Entities;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+namespace APItestStudent.Services
+{
+    public interface IService<Entity>
+    {
+        public void Add(Entity value);
+        public void Update(Entity value);
+        public Entity GetById(Guid id);
+        public bool Remove(Guid id);
+        public IEnumerable<Entity> GetAll();
+    }
+    public class Service : IService<Student>
+    {
+        List<Student> _studentContest = new List<Student>();
+
+        public void Add(Student value)
+        {
+            value.Id = Guid.NewGuid();
+            _studentContest.Add(value);
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return _studentContest;
+        }
+
+        public Student GetById(Guid id)
+        {
+            return _studentContest.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        public bool Remove(Guid id)
+        {
+            _studentContest.Remove(_studentContest.Where(x => x.Id == id).FirstOrDefault());
+                return true;
+        }
+
+        public void Update(Student value)
+        {
+            var obj = _studentContest.FirstOrDefault(x => x.Id == value.Id);
+            if (obj != null) {
+                obj.Name = value.Name;
+                obj.Description = value.Description;
+            }
+            Console.WriteLine("no way");
+        }
+    }
+}
